89bb035861a21a1ba7a22c2026b1b78090c9cd38
hyve9 <afb8252@nyu.edu>
2024-03-25 16:24:19 -0400

    silent download

--- download-mir-dataset.py
+++ download-mir-dataset.py
@@ -1,14 +1,29 @@
+import os
+import sys
 import argparse
 import mirdata
 
-def download_dataset(dataset_name):
-    data = mirdata.initialize(dataset_name, data_home=f'./mir_datasets/{dataset_name}')
-    data.download(force_overwrite=False)
+def download_dataset(dataset, silent=False):
+    # Save the original stdout
+    original_stdout = sys.stdout
+
+    if silent:
+        # Set stdout to null
+        sys.stdout = open(os.devnull, 'w')
+
+    try:
+        data = mirdata.initialize(dataset, data_home=f'./mir_datasets/{dataset}')
+        data.download(force_overwrite=False)
+    finally:
+        # Restore the original stdout
+        sys.stdout = original_stdout
+    return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download an MIR dataset.')
     parser.add_argument('--dataset', required=True, type=str, help='The name of the MIR dataset to download')
+    parser.add_argument('--silent', action='store_true', help='Suppress stdout output')
 
     args = parser.parse_args()
 
-    download_dataset(args.dataset)
+    download_dataset(args.dataset, args.silent)
